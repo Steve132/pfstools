a2dc20b50d93b3a470713c87d354a127bb509bf7
Steve132 <steve@soapforge.com>
2015-08-25 18:58:04 -0400

    more code towards pfspanoramic

--- src/filter/pfspanoramic_new.cpp
+++ src/filter/pfspanoramic_new.cpp
@@ -49,7 +49,7 @@ struct projection_size
 	int width;
 	int height;
 	int latitude_frequency;
-	projection_size():width(-1),height(-1),latitude_frequency(-1)
+	projection_size(int w=-1,int h=-1,int l=-1):width(w),height(h),latitude_frequency(l)
 	{}
 };
 class Projection
@@ -60,6 +60,7 @@ public:
 	
 	virtual bool is_valid_pixel(double u, double v) const = 0;
 	virtual projection_size get_projection_size(const projection_size& options=projection_size()) const = 0;
+	virtual std::string guess_valid_string(int w,int h) const = 0;
 
 	virtual ~Projection()
 	{}
@@ -137,12 +138,168 @@ public:
 		found->second->setOptions(options);
 		return found->second;
 	}
+	std::string guess_projstring(int x,int y)
+	{
+		if(x <= 0 || y <= 0)
+		{
+			throw std::runtime_error("No projection can be determined automatically from the given size parameters.  Please specify a projection or size parameters");
+		}
+		std::string guess="";
+		for(map<std::string,Projection*>::iterator it=projections.begin();it!=projections.end();++it)
+		{
+			std::string cguess=it->second->guess_projstring(x,y);
+			if(cguess != "")
+			{
+				if(guess=="")
+				{
+					guess=cguess;
+				}
+				else
+				{
+					throw std::runtime_error(std::string("Cannot determine projection automatically from size parameters..given parameters could be ")+cguess+" or "+guess);
+				}
+			}
+		}
+		if(guess=="")
+		{
+			throw std::runtime_error("No projection could be determined automatically from the given size parameters.  Please specify a projection.");
+		}
+	}
 };
 
-int panoramic()
+static const std::string PROG_NAME="pfspanoramic";
+void printHelp(const std::string& arg0)
 {
+  cerr << arg0 << " <source projection>+<target projection> [--width <val>] [--height <val>] [--oversample <val>] [--interpolate] [--xrotate <angle>] [--yrotate <angle>] [--zrotate <angle>] [--verbose] [--help]\n" 
+	<< "See man page for more information.\n";
+}
+struct cmdargs
+{
+	std::string pstring="";
+	projection_size size;
+	bool verbose=false;
+	Vector3D rotation;
+	bool interpolate=false;
+	int oversample=1;
+	
+	cmdargs(const std::vector<std::string>& args):
+		pstring(""),
+		verbose(false),
+		interpolate(false),
+		oversample(false)
+	{
+		for(int i=1;i<args.size();i++)
+		{
+			if(args[i][0]!='-')
+			{
+				pstring=args[i];
+			}
+			else if(args[i]=='--help')
+			{
+				printHelp(args[0]);
+				return 0;
+			}
+			else
+			{
+				char ch=args[i][1]=='-' ? args[i][2] : args[i][1];
+				switch(ch)
+				{
+				case 'w':
+				{
+					std::istringstream(args[++i]) >> size.width;
+					break;
+				}
+				case 'h':
+				{
+					std::istringstream(args[++i]) >> size.height;
+					break;
+				}
+				case 'x':
+				{
+					std::istringstream(args[++i]) >> rotation.x;
+					break;
+				}
+				case 'y':
+				{
+					std::istringstream(args[++i]) >> rotation.y;
+					break;
+				}
+				case 'z':
+				{
+					std::istringstream(args[++i]) >> rotation.z;
+					break;
+				}
+				case 'i':
+				{
+					interpolate=true;
+					break;
+				}
+				case 'o':
+				{
+					std::istringstream(args[++i]) >> oversample;
+					break;
+				}
+				default:
+				{
+					printHelp(args[0]);
+					throw std::runtime_error("Invalid arguments");
+				}
+				};
+			}
+		}
+		if(pstring == "")
+		{
+			throw std::runtime_error("No projection string detected!");
+		}
+		if(oversample < 1)
+		{
+			throw std::runtime_error("Oversampling must be an integer greater than 10");
+		}
+	}
+};
+
+Projection* replace_guess(Projection* proj,const std::string pstr,ProjectionRegistry& reg,int width,int height)
+{
+	if(proj!=NULL)
+		return proj;
+	
+	if(pstr=="")
+	{
+		pstr=reg.guess_projstring(width,height);
+	}
+	return inprojections.parse_projstring(pstr);
+}
+
+int panoramic(const std::vector<std::string>& args)
+{
+	cmdargs argsin(args);
 	ProjectionRegistry inprojections,outprojections;
+	
+	pfs::DOMIO pfsio;
+	
+	
 	std::vector<std::string> inoutpstring=split(pstring,"+");
+	
+	Projection* inproj=NULL;
+	Projection* outproj=NULL;
+	
+	pfs::Frame *inframe;
+	while(inframe=pfsio.readFrame( stdin ))
+	{
+		inproj=replace_guess(inproj,inoutpstring[0],inprojections,inframe->getWidth(),inframe->getHeight());
+		projection_size ips=inproj->get_projection_size(projection_size(inframe->getWidth(),inframe->getHeight()));
+		outproj=replace_guess(outproj,inoutpstring[1],outprojections,argsin.size.x,argin.size.y);
+		
+		projection_size ops;
+		if(argin.size.x <=0 || argin.size.y <=0)
+		{
+			ops=outproj->get_projection_size(ips);
+		}
+		else
+		{
+			ops=argin.size;
+		}
+	}
 }
 
 
